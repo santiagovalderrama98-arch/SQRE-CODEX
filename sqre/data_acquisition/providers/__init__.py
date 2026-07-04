210e08831402159085b345d22770483537c33512
santiagovalderrama98-arch <santiagovalderrama98@hotmail.com>
2026-07-04 16:08:15 +0000

    Add Twelve Data and MT5 providers

--- sqre/data_acquisition/providers/__init__.py
+++ sqre/data_acquisition/providers/__init__.py
@@ -2,5 +2,7 @@
 
 from sqre.data_acquisition.providers.dukascopy_provider import DukascopyProvider
 from sqre.data_acquisition.providers.histdata_provider import HistDataProvider
+from sqre.data_acquisition.providers.mt5_provider import MT5Provider
+from sqre.data_acquisition.providers.twelvedata_provider import TwelveDataProvider
 
-__all__ = ["DukascopyProvider", "HistDataProvider"]
+__all__ = ["DukascopyProvider", "HistDataProvider", "MT5Provider", "TwelveDataProvider"]
